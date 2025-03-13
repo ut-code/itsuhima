b29d82e8a167c25669059b2c872b7048d7dc1aef
Shogo Nakamura <104970808+naka-12@users.noreply.github.com>
2025-03-13 12:58:35 +0900

    カレンダーで矩形選択 (#2)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,84 @@
-// import { UserSchema } from "../../common/schema";
-// import { z } from "zod";
-// import FullCalendar from "@fullcalendar/react";
-// import timeGridPlugin from "@fullcalendar/timegrid";
+import { UserSchema } from "../../common/schema";
+import { z } from "zod";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { useRef } from "react";
 
 import { NavLink } from "react-router";
 
-// async function sampleFetch() {
-//   const response = await fetch("http://localhost:3000/users/");
-//   const result = await response.json();
-//   const data = z.array(UserSchema).parse(result);
-//   data.forEach((item) => console.log("name: ", item.name, "age: ", item.age));
-// }
+async function sampleFetch() {
+  const response = await fetch("http://localhost:3000/users/");
+  const result = await response.json();
+  const data = z.array(UserSchema).parse(result);
+  data.forEach((item) => console.log("name: ", item.name, "age: ", item.age));
+}
 
-// async function sampleEventFetch() {
-//   const response = await fetch("http://localhost:3000/sample/events");
-//   const result = await response.json();
-//   console.log(result);
-// }
+async function sampleEventFetch() {
+  const response = await fetch("http://localhost:3000/sample/events");
+  const result = await response.json();
+  console.log(result);
+}
 
 function App() {
+  const calendarRef = useRef<FullCalendar | null>(null);
   return (
     <>
       <h1 className="text-4xl">トップページ</h1>
       <NavLink to="/create" end>
         イベントを作成する。
       </NavLink>
-      {/* <FullCalendar plugins={[timeGridPlugin]} />
+      <FullCalendar plugins={[timeGridPlugin]} />
+      <h1 className="text-4xl">Vite + React</h1>
+      <FullCalendar
+        ref={calendarRef}
+        plugins={[timeGridPlugin, interactionPlugin]}
+        longPressDelay={200}
+        selectable={true}
+        selectAllow={
+          // https://github.com/fullcalendar/fullcalendar/issues/4119#issuecomment-875328866
+          (info) => {
+            console.log("selecting " + info.startStr + " to " + info.endStr, calendarRef);
+            if (!calendarRef.current) {
+              console.log("no calendar ref");
+              return false;
+            }
+            const calendarApi = calendarRef.current.getApi();
+            const existing = calendarApi.getEventById("selectBox");
+            if (existing) {
+              existing.remove();
+            }
+            let startTime = info.start.toLocaleTimeString("ja-JP", {
+              hour: "2-digit",
+              minute: "2-digit",
+            });
+            let endTime = info.end.toLocaleTimeString("ja-JP", {
+              hour: "2-digit",
+              minute: "2-digit",
+            });
+
+            if (
+              info.start.getHours() > info.end.getHours() ||
+              (info.start.getHours() === info.end.getHours() &&
+                info.start.getMinutes() > info.end.getMinutes())
+            ) {
+              [startTime, endTime] = [endTime, startTime];
+            }
+
+            calendarApi.addEvent({
+              id: "selectBox",
+              startTime: startTime,
+              endTime: endTime,
+              startRecur: info.start,
+              endRecur: info.end,
+            });
+            return false;
+          }
+        }
+        select={(info) => {
+          console.log("selected " + info.startStr + " to " + info.endStr);
+        }}
+      />
       <div>
         <button className="btn" onClick={sampleFetch}>
           fetch
@@ -33,7 +86,7 @@ function App() {
         <button className="btn" onClick={sampleEventFetch}>
           events
         </button>
-      </div> */}
+      </div>
     </>
   );
 }
