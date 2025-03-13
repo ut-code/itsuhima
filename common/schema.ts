3dc4ca2520774dc48a07f513030ff896ba81c20a
KaichiManabe <154493010+KaichiManabe@users.noreply.github.com>
2025-03-13 14:15:18 +0900

    change schema

--- common/schema.ts
+++ common/schema.ts
@@ -1,8 +1,16 @@
 import { z } from "zod";
 
 export const UserSchema = z.object({
-    name: z.string(),
-    age: z.number(),
+  name: z.string(),
+  age: z.number(),
 });
 
-export type User = z.infer<typeof UserSchema>
+export type User = z.infer<typeof UserSchema>;
+
+export const SlotSchema = z.object({
+  id: z.string().uuid(),
+  eventId: z.string().uuid(),
+  guestId: z.string().uuid(),
+  start: z.string().datetime(),
+  end: z.string().datetime(),
+});
