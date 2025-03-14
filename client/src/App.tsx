7a5f43801f28e933eae55d777bf14e98fea1f308
Shogo Nakamura <104970808+naka-12@users.noreply.github.com>
2025-03-14 09:19:18 +0900

    複数イベントの登録 UI (#3)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { z } from "zod";
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import dayjs from "dayjs";
 import { useRef } from "react";
 
 import { NavLink } from "react-router";
@@ -20,8 +21,97 @@ async function sampleEventFetch() {
   console.log(result);
 }
 
+function editSlots(
+  from: Date,
+  to: Date,
+  calendarRef: React.RefObject<FullCalendar | null>,
+  slotsRef: React.RefObject<{ from: Date; to: Date }[]>,
+  isDeletion: boolean
+) {
+  if (!calendarRef.current) {
+    console.log("no calendar ref");
+    return;
+  }
+  const calendarApi = calendarRef.current.getApi();
+
+  const start = dayjs(from);
+  const end = dayjs(to);
+
+  const dateDiff = end.startOf("day").diff(start.startOf("day"), "day") + 1;
+  const needReverse = start.format("HH:mm") > end.format("HH:mm");
+
+  const startHour = !needReverse ? start.hour() : end.hour();
+  const startMinute = !needReverse ? start.minute() : end.minute();
+  const endHour = !needReverse ? end.hour() : start.hour();
+  const endMinute = !needReverse ? end.minute() : start.minute();
+
+  if (!isDeletion) {
+    // 各日付ごとに分解して Slot に追加
+    for (let i = 0; i < dateDiff; i++) {
+      console.log(dateDiff);
+      const base = start.add(i, "day");
+
+      const slotStart = base.hour(startHour).minute(startMinute);
+      const slotEnd = base.hour(endHour).minute(endMinute);
+
+      console.log(slotStart.toDate(), slotEnd.toDate());
+
+      // TODO: 重複した場合追加を行わない
+      slotsRef.current.push({
+        from: slotStart.toDate(),
+        to: slotEnd.toDate(),
+      });
+
+      calendarApi.addEvent({
+        start: slotStart.toDate(),
+        end: slotEnd.toDate(),
+        display: "background",
+        color: "red",
+      });
+    }
+  } else {
+    // TODO: 削除処理
+  }
+
+  // 選択範囲をクリア
+  const existing = calendarApi.getEventById("selectBox");
+  if (existing) {
+    existing.remove();
+  }
+}
+
 function App() {
   const calendarRef = useRef<FullCalendar | null>(null);
+
+  const slotsRef = useRef<
+    {
+      from: Date;
+      to: Date;
+    }[]
+  >([]);
+
+  const hoveringEventRef = useRef<{ from: Date; to: Date } | null>(null);
+  const deletionStartRef = useRef<{ from: Date; to: Date } | null>(null);
+
+  const handleDragStart = () => {
+    if (hoveringEventRef.current) {
+      console.log("✨", "deletionStartRef continued!", hoveringEventRef.current);
+      deletionStartRef.current = { ...hoveringEventRef.current };
+    }
+  };
+
+  const handleDragEnd = () => {
+    if (deletionStartRef.current && hoveringEventRef.current) {
+      console.log("🧨", "delete", deletionStartRef.current, hoveringEventRef.current);
+    }
+  };
+  document.onmousedown = handleDragStart;
+  document.onmouseup = handleDragEnd;
+
+  // TODO: スマホで動かない・・・？
+  // document.onpointerdown = handleDragStart;
+  // document.onpointerup = handleDragEnd;
+
   return (
     <>
       <h1 className="text-4xl">トップページ</h1>
@@ -32,11 +122,25 @@ function App() {
         ref={calendarRef}
         plugins={[timeGridPlugin, interactionPlugin]}
         longPressDelay={200}
+        slotDuration={"00:15:00"}
+        eventMouseEnter={(info) => {
+          if (info.event.start && info.event.end) {
+            hoveringEventRef.current = { from: info.event.start, to: info.event.end };
+          }
+        }}
+        eventMouseLeave={() => {
+          hoveringEventRef.current = null;
+        }}
+        // TODO: 単一セルだとダメかもしれない・・・
         selectable={true}
         selectAllow={
-          // https://github.com/fullcalendar/fullcalendar/issues/4119#issuecomment-875328866
+          // https://github.com/fullcalendar/fullcalendar/issues/4119
           (info) => {
-            console.log("selecting " + info.startStr + " to " + info.endStr, calendarRef);
+            let selectionColor = "green";
+            if (deletionStartRef.current) {
+              console.log("🔥", deletionStartRef.current);
+              selectionColor = "blue";
+            }
             if (!calendarRef.current) {
               console.log("no calendar ref");
               return false;
@@ -69,15 +173,32 @@ function App() {
               endTime: endTime,
               startRecur: info.start,
               endRecur: info.end,
+              display: "background",
+              color: selectionColor,
             });
-            return false;
+            return true;
           }
         }
         select={(info) => {
-          console.log("selected " + info.startStr + " to " + info.endStr);
+          editSlots(info.start, info.end, calendarRef, slotsRef, !!deletionStartRef.current);
+          deletionStartRef.current = null;
+          hoveringEventRef.current = null;
         }}
       />
       <div>
+        <button
+          onClick={() => {
+            alert(
+              slotsRef.current
+                .map((slot) => {
+                  return `${slot.from.toLocaleString()} - ${slot.to.toLocaleString()}`;
+                })
+                .join("\n"),
+            );
+          }}
+        >
+          イベントを表示
+        </button>
         <button className="btn" onClick={sampleFetch}>
           fetch
         </button>
