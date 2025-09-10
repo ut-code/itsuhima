8587ff81e4587416e60f33a3ff920cfd8623ca40
nakaterm <104970808+nakaterm@users.noreply.github.com>
2025-09-10 23:34:18 +0900

    refactor: uninstall express

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,15 +1,11 @@
 import { serve } from "@hono/node-server";
+import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
-import { deleteCookie, getCookie, setCookie } from "hono/cookie";
 import { cors } from "hono/cors";
+import projectsRoutes from "./routes/projects.js";
 
 import dotenv from "dotenv";
 dotenv.config();
-import { PrismaClient } from "@prisma/client";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import express, { type CookieOptions } from "express";
-import projectsRoutes from "./routes/projects.js";
 
 export const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
